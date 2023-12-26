2dfc5e2fe7cc31df247500eab54d21dccf083e73
B4PT0R <bferrand.maths@gmail.com>
2023-12-26 22:46:56 +0100

    Initial commit

---
+++ streamlit_modal_input/frontend/src/App.tsx
@@ -0,0 +1,132 @@
+import React from "react";
+import { Streamlit, StreamlitComponentBase, withStreamlitConnection } from "streamlit-component-lib";
+import { initializeApp, FirebaseApp } from "firebase/app";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+import tinycolor from 'tinycolor2'
+import './App.css';
+
+interface State {
+  inputText: string;
+  firebaseInitialized: boolean;
+  firebaseApp:FirebaseApp | null;
+  enabled: boolean;
+  hasFocus: boolean;
+}
+
+class ModalInput extends StreamlitComponentBase<State> {
+
+    private inputRef: React.RefObject<HTMLInputElement>;
+
+    constructor(props:any) {
+        super(props);
+        // Initialize the ref
+        this.inputRef = React.createRef<HTMLInputElement>();
+    }
+
+    public state = {
+        inputText: "",
+        firebaseInitialized: false,
+        firebaseApp: null,
+        enabled: false,
+        hasFocus: false,
+    };
+
+    componentDidMount() {
+        if (!this.state.firebaseInitialized && this.props.args["firebase_config"]) {
+            const app=initializeApp(this.props.args["firebase_config"]);
+            this.setState({ firebaseInitialized: true, firebaseApp: app });
+        }
+        if (this.inputRef.current) {
+            this.inputRef.current.focus();
+        }
+        this.setState({enabled:this.props.args["enabled"],inputText:this.props.args["value"]})
+    }
+
+    handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ inputText: event.target.value });
+    };
+
+    handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        // Check if Enter is pressed
+        if (event.key === 'Enter') {
+          this.handleSubmit();
+        }
+    };
+
+    handleSubmit = async () => {
+        const firestoreCollection=this.props.args["collection"];
+        const firestoreDocument = this.props.args["document"];
+        
+        if (this.state.firebaseApp) {
+            const db = getFirestore(this.state.firebaseApp);
+            const inputRef = doc(db, firestoreCollection, firestoreDocument);
+            const output={
+                content: this.state.inputText,
+                Id: Math.floor(Date.now() / 1000)
+            };
+            try {
+                await setDoc(inputRef, output);
+                this.setState({ enabled: false });
+                Streamlit.setComponentValue(this.state.inputText);
+            } catch (error) {
+                console.error("Error writing to Firestore: ", error);
+            }
+        } else {
+            console.error("Firebase App is not initialized");
+        }
+      };
+
+    private TextInputStyle = (Theme:any):any => {
+        const Height=21
+        const baseBorderColor = tinycolor.mix(Theme.textColor, Theme.backgroundColor, 80).lighten(2).toString();
+        const backgroundColor = tinycolor.mix(Theme.textColor, tinycolor.mix(Theme.primaryColor, Theme.backgroundColor, 99), 99).lighten(0.5).toString();
+        const textColor = Theme.textColor;
+        const borderColor = this.state.hasFocus && this.state.enabled ? Theme.primaryColor : baseBorderColor
+        const padding=12;
+        
+        return {
+            height: `${Height+2*padding}px`,
+            borderColor: borderColor,
+            backgroundColor: backgroundColor,
+            color: textColor,
+            whiteSpace: 'pre',
+            overflowX: 'auto',
+            resize:"none",
+            width: 'calc(100% - 6px)',
+            margin: '3px',
+            outline: 'none',
+            fontFamily: "monospace",
+            padding:`${padding}px 6px`
+        };
+    };
+
+    public render = (): React.ReactNode => {
+        const Theme = this.props.theme ?? {
+            base: 'dark',
+            backgroundColor: 'black',
+            secondaryBackgroundColor: 'grey',
+            primaryColor: 'red',
+            textColor: 'white'
+          };
+        Streamlit.setComponentValue(this.state.inputText)
+        return (
+            <div className="modal-input">
+            <input
+                className="mytextinput"
+                ref={this.inputRef}
+                type="text"
+                value={this.state.inputText}
+                placeholder={this.props.args["prompt"]}
+                onChange={this.handleInputChange}
+                onKeyDown={this.handleKeyDown}
+                disabled={!this.state.enabled}
+                style={this.TextInputStyle(Theme)}
+                onFocus={() => this.setState({ hasFocus: true })}
+                onBlur={() => this.setState({ hasFocus: false })}
+            />
+            </div>
+        );
+    };
+}
+
+export default withStreamlitConnection(ModalInput);
---
+++ streamlit_modal_input/frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import ModalInput from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ModalInput />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
